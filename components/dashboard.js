25e7629db752a339fa6ff66359fbdd5cb0b9a2ea
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-08 16:23:57 +0800

    Update dashboard.js

--- components/dashboard.js
+++ components/dashboard.js
@@ -556,8 +556,9 @@ export const Dashboard = ({ updateBalance, balance }) => {
                   </Stack>
 
                   {/* Displaying Coin */}
-                  <Stack sx={{ height: '190px' }}>
-                    <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQrVsZWJvvmyIPf710YDerz8LqUFXbiiRT4DhQoheeylbvqi6y_AAUfL4RJqqE2qA/pubchart?oid=485486164&amp;format=interactive"></iframe>
+                  <Stack sx={{ height: '600px' }}>
+                    <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQrVsZWJvvmyIPf710YDerz8LqUFXbiiRT4DhQoheeylbvqi6y_AAUfL4RJqqE2qA/pubchart?oid=485486164&amp;format=interactive">
+                    </iframe>
                   </Stack>
 
                   {/* Select Tails Button */}
