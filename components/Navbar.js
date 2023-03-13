6c0f84783768e781a5fc0297bf8c30fba16e03e9
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-13 14:18:24 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({balance}) => {
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
         <div>        
       <a href="https://the-hidden-valley.gitbook.io/solympusmob/" target="_blank" rel="noreferrer">
-        <img src='https://avatars.githubusercontent.com/u/7111340?s=280&v=4'></img>
+        <img src='https://avatars.githubusercontent.com/u/7111340?s=280&v=4' height="10" width="10"></img>
       </a>  
         </div>
           <Typography
