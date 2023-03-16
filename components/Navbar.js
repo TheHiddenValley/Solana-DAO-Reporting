fc59580b76299351ea9d4f1c9307dd83bb5ef6f8
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-16 14:23:54 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,10 +33,7 @@ const Navbar = ({balance}) => {
             <img src='https://cdn.discordapp.com/attachments/1075476230637375528/1082941273201197107/logo.png' className='w-100'></img>
         </div>
       </a> 
-    
-    <script> src="https://www.scmplayer.net/script.js" 
-data-config="{'skin':'skins/black/skin.css','volume':50,'autoplay':true,'shuffle':false,'repeat':1,'placement':'top','showplaylist':false,'playlist':[{'title':'Lore Master by Russ a Buss','url':'https://www.youtube.com/watch?v=rBiI6dJxfro&embeds_euri=https%3A%2F%2Fsolympusmob.xyz%2F&source_ve_path=MjM4NTE&feature=emb_title&ab_channel=RussaBuss'}]}" ></script>
-
+   
     
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
     
