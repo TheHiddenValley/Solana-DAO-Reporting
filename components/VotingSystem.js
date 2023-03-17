81054a3f51405be06d2adcbbcf0f40a1f1c45931
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-17 15:53:54 +0800

    Update VotingSystem.js

--- components/VotingSystem.js
+++ components/VotingSystem.js
@@ -15,26 +15,26 @@ function VotingSystem() {
   };
 
   return (
-    <div className="voting-system">
-      <h style="color:white;font-size:32px;">Vote for your favorite option:</h>
-      <div className="options">
-        <div className="option">
-          <img src="https://www.mobstudios.io/kenzo.jpg" height="20" width="20" alt="Option 1" onClick={() => handleVote("option1")} />
-          <p style="color:white;font-size:32px;">Kenzo</p>
-          <p style="color:white;font-size:32px;">Votes: {votes.option1}</p>
-        </div>
-        <div className="option">
-          <img src="https://www.mobstudios.io/hal.png" height="20" width="20" alt="Option 2" onClick={() => handleVote("option2")} />
-          <p style="color:white;font-size:32px;">Hal</p>
-          <p style="color:white;font-size:32px;">Votes: {votes.option2}</p>
-        </div>
-        <div className="option">
-          <img src="https://www.mobstudios.io/uzy.png" height="20" width="20" alt="Option 3" onClick={() => handleVote("option3")} />
-          <p style="color:white;font-size:32px;">Uzy</p>
-          <p style="color:white;font-size:32px;">Votes: {votes.option3}</p>
-        </div>
-      </div>
+<div className="voting-system">
+  <h1 style={{ color: "white", fontSize: "32px" }}>Vote for your favorite option:</h1>
+  <div className="options">
+    <div className="option">
+      <img src="https://www.mobstudios.io/kenzo.jpg" height="20" width="20" alt="Option 1" onClick={() => handleVote("option1")} />
+      <p style={{ color: "white", fontSize: "32px" }}>Kenzo</p>
+      <p style={{ color: "white", fontSize: "32px" }}>Votes: {votes.option1}</p>
     </div>
+    <div className="option">
+      <img src="https://www.mobstudios.io/hal.png" height="20" width="20" alt="Option 2" onClick={() => handleVote("option2")} />
+      <p style={{ color: "white", fontSize: "32px" }}>Hal</p>
+      <p style={{ color: "white", fontSize: "32px" }}>Votes: {votes.option2}</p>
+    </div>
+    <div className="option">
+      <img src="https://www.mobstudios.io/uzy.png" height="20" width="20" alt="Option 3" onClick={() => handleVote("option3")} />
+      <p style={{ color: "white", fontSize: "32px" }}>Uzy</p>
+      <p style={{ color: "white", fontSize: "32px" }}>Votes: {votes.option3}</p>
+    </div>
+  </div>
+</div>
   );
 }
 
