0da01afd45078e8d323261f63ca914bc6b2c5f36
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-17 16:46:36 +0800

    Update dashboard.js

--- components/dashboard.js
+++ components/dashboard.js
@@ -596,27 +596,29 @@ export const Dashboard = ({ updateBalance, balance }) => {
                   sx={{ width: '100%' }}
                 >
                   {/* Main Pie Chart */}
-                <Stack>
-<iframe width="auto" height="380" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTenhHYcAY7N4rM6bGeYupReT6yY5DaogD7vMsJa8Jn8DvbDmdEaXIb_n75qzZTltSPD0obpjrVxGsM/pubchart?oid=1855293623&amp;format=interactive"></iframe>                  
-<style>
-  /* Adjust the font size and color of the text */
-  body {
-    font-size: 16px;
-    color: #333;
-  }
+<Stack>
+  <div className="sheet-container">
+    <iframe title="Google Sheet" width="100%" height="380" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTenhHYcAY7N4rM6bGeYupReT6yY5DaogD7vMsJa8Jn8DvbDmdEaXIb_n75qzZTltSPD0obpjrVxGsM/pubchart?oid=1855293623&amp;format=interactive"></iframe>
+  </div>
+  <style>
+    /* Adjust the font size and color of the text */
+    body {
+      font-size: 16px;
+      color: #333;
+    }
   
-  /* Add padding and margins to the sheet */
-  .sheet-container {
-    padding: 20px;
-    margin: 0 auto;
-    max-width: 960px;
-  }
+    /* Add padding and margins to the sheet */
+    .sheet-container {
+      padding: 20px;
+      margin: 0 auto;
+      max-width: 960px;
+    }
   
-  /* Hide the sheet's header row */
-  .sheet-header-row {
-    display: none;
-  }
-</style>                
+    /* Hide the sheet's header row */
+    .sheet-header-row {
+      display: none;
+    }
+  </style>                
 </Stack>
                   {/* Sharky.fi Profit Chart */}
                 <Stack>
