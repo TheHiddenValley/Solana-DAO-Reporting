adc89b7668d92f9d78717b8a61a240a7f21ebb46
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2023-03-17 14:58:24 +0800

    Create VotingSystem.js

---
+++ components/VotingSystem.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function VotingSystem() {
+  const [votes, setVotes] = useState({
+    option1: 0,
+    option2: 0,
+    option3: 0
+  });
+
+  const handleVote = (option) => {
+    setVotes(prevState => ({
+      ...prevState,
+      [option]: prevState[option] + 1
+    }));
+  };
+
+  return (
+    <div className="voting-system">
+      <h2>Vote for your favorite option:</h2>
+      <div className="options">
+        <div className="option">
+          <img src="option1.png" alt="Option 1" onClick={() => handleVote("option1")} />
+          <p>Option 1</p>
+          <p>Votes: {votes.option1}</p>
+        </div>
+        <div className="option">
+          <img src="option2.png" alt="Option 2" onClick={() => handleVote("option2")} />
+          <p>Option 2</p>
+          <p>Votes: {votes.option2}</p>
+        </div>
+        <div className="option">
+          <img src="option3.png" alt="Option 3" onClick={() => handleVote("option3")} />
+          <p>Option 3</p>
+          <p>Votes: {votes.option3}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VotingSystem;
